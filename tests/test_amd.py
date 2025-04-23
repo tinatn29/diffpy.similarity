d6ac6eaa9963edb4dec17d6378cf58a9f3e073c4
tinatn29 <tanaporn.nanarong@gmail.com>
2025-04-23 15:53:59 -0400

    edit tests

--- tests/test_amd.py
+++ tests/test_amd.py
@@ -1,12 +1,15 @@
 import pytest
 from pathlib import Path
+import pandas as pd
 from diffpy.similarity.metrics.amd import amd_compare, pdd_compare
 
 
 # get cif files for tests
 curr_path = Path().absolute()
-cif1 = curr_path / 'test_data' / 'mp-390.cif'
-cif2 = curr_path / 'test_data' / 'mp-458.cif'
+cif1 = curr_path / 'tests'/ 'test_data' / 'mp-390.cif'
+cif2 = curr_path / 'tests'/ 'test_data' / 'mp-458.cif'
+dm_amd = pd.read_pickle(curr_path / 'tests'/ 'test_data' / 'dm_amd.pkl')
+dm_pdd = pd.read_pickle(curr_path / 'tests'/ 'test_data' / 'dm_pdd.pkl')
 
 # set up input and output for tests
 # for each case: (input1, input2, expected_output)
@@ -14,18 +17,9 @@ amd_test_datasets = [
     # test identical cif files
     (cif1, cif1, 0.0),
     # test different cif files
-    (cif1, cif2, dm(cif1, cif2)),
+    (cif1, cif2, 0.5725326132426836),
     # test generate a distance matrix from two lists
-    ([cif1, cif2], [cif1, cif2], dm)
-]
-
-pdd_test_datasets = [
-    # test identical cif files
-    (cif1, cif1, 0.0),
-    # test different cif files
-    (cif1, cif2, dm(cif1, cif2)),
-    # test generate a distance matrix from two lists
-    ([cif1, cif2], [cif1, cif2], dm)
+    ([cif1, cif2], [cif1, cif2], dm_amd)
 ]
 
 @pytest.mark.parametrize("input1, input2, expected_output", amd_test_datasets)
@@ -33,13 +27,33 @@ def test_amd_compare(input1, input2, expected_output):
     """
     Test the amd_compare function.
     """
-    result = amd_compare(input1, input2)
-    assert result == expected_output
+    result = amd_compare(input1, input2, k=100)
+    if isinstance(input1, list) or isinstance(input2, list):
+        # check if two dataframes are equal
+        assert result.equals(expected_output)
+    else:
+        # check if two floats are equal
+        assert result == expected_output
+
+# for each case: (input1, input2, expected_output)
+pdd_test_datasets = [
+    # test identical cif files
+    (cif1, cif1, 0.0),
+    # test different cif files
+    (cif1, cif2, 0.6675364987310654),
+    # test generate a distance matrix from two lists
+    ([cif1, cif2], [cif1, cif2], dm_pdd)
+]
 
 @pytest.mark.parametrize("input1, input2, expected_output", pdd_test_datasets)
 def test_pdd_compare(input1, input2, expected_output):
     """
     Test the pdd_compare function.
     """
-    result = pdd_compare(input1, input2)
-    assert result == expected_output
+    result = pdd_compare(input1, input2, k=100)
+    if isinstance(input1, list) or isinstance(input2, list):
+        # check if two dataframes are equal
+        assert result.equals(expected_output)
+    else:
+        # check if two floats are equal
+        assert result == expected_output
